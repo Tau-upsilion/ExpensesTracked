61a6e9a642751b67b87997b21d825231ac3b33d1
Ty <txtrinh@iastate.edu>
2019-02-16 16:17:30 -0600

    created backend folder

---
+++ Experiments/Ty/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/ExpensesTrackedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExpensesTracked_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTrackedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTrackedBackendApplication.class, args);
+	}
+
+}
