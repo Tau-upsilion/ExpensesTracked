cc1f5a742c94b6c5a92c541a77a2442f322cb6e4
Jacob Gleason <jggleas@iastate.edu>
2019-02-18 15:42:28 -0600

    updates

--- Experiments/Jacob/HelloAndroid/app/src/main/java/com/example/hello_android/MessageActivity.java
+++ Experiments/Jacob/HelloAndroid/app/src/main/java/com/example/hello_android/MessageActivity.java
@@ -1,13 +1,45 @@
 package com.example.hello_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
+
+    Button sendManager, sendIntent;
+    TextView smsMessage, smsPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        sendManager = findViewById(R.id.smsManager);
+        sendIntent = findViewById(R.id.smsSIntent);
+        smsMessage = findViewById(R.id.smsBody);
+        smsPhoneNumber = findViewById(R.id.phoneNumber);
+
+        sendManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(smsPhoneNumber.getText().toString(), null, smsMessage.getText().toString(), null, null);
+            }
+        });
+
+        sendIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendI = new Intent(Intent.ACTION_VIEW);
+                sendI.putExtra(smsMessage.getText().toString(), smsPhoneNumber.getText().toString());
+                sendI.setType("vnd.android-dir/mms-sms");
+                startActivity(sendI);
+            }
+        });
     }
+
 }
