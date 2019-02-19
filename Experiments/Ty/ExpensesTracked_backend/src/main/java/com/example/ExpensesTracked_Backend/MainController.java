8daef1b1163b93cb4f3471908b1292e42812f73e
Ty <txtrinh@iastate.edu>
2019-02-18 21:29:08 -0600

    added more user variables and a POST request to add users

--- Experiments/Ty/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/MainController.java
+++ Experiments/Ty/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/MainController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -35,5 +38,14 @@ public class MainController {
 	public @ResponseBody Iterable<User> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return userRepository.findAll();
+	}
+	@GetMapping("/users/{id}")
+	User one(@PathVariable Integer id) {
+		return userRepository.findById(id)
+				.orElseThrow();
+	}
+	@PostMapping("/users/add")
+	public User create(@RequestBody User newUser) {
+		return newUser;
 	}
 }
--- Experiments/Ty/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/User.java
+++ Experiments/Ty/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/User.java
@@ -13,6 +13,31 @@ public class User {
     private String name;
 
     private String email;
+    
+    private String password;
+    
+    private Integer age;
+    
+    private String gender;
+    
+    public void setGender(String gender) {
+    	this.gender = gender;
+    }
+    public String getGender() {
+    	return gender;
+    }
+    public void setAge(Integer age) {
+    	this.age = age;
+    }
+    public Integer getAge() {
+    	return age;
+    }
+    public String getPassword() {
+    	return password;
+    }
+    public void setPassword(String password) {
+    	this.password = password;
+    }
 
 	public Integer getId() {
 		return id;
