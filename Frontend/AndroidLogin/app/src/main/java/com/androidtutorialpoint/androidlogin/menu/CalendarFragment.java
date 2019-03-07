acdc3d87d5af5b8f93b540b4ba39c7c219f34433
Jacob Gleason <jggleas@iastate.edu>
2019-03-06 21:10:06 -0600

    added a calendar view

--- Frontend/AndroidLogin/app/src/main/java/com/androidtutorialpoint/androidlogin/menu/CalendarFragment.java
+++ Frontend/AndroidLogin/app/src/main/java/com/androidtutorialpoint/androidlogin/menu/CalendarFragment.java
@@ -7,17 +7,78 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.androidtutorialpoint.androidlogin.R;
 
 public class CalendarFragment extends Fragment {
+    // Instance variables
+    String selectedDate;
+    TextView date;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Variables
         View v = inflater.inflate(R.layout.fragment_calendar, null);
+        CalendarView calendar;
 
+        // Initializations
+        calendar = v.findViewById(R.id.cal_calendar);
+        date = v.findViewById(R.id.cal_date);
+
+        // OnDateChange Listener
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                switch(month)
+                {
+                    case 0:
+                        selectedDate = "Jan " + dayOfMonth + ", " + year;
+                        break;
+                    case 1:
+                        selectedDate = "Feb " + dayOfMonth + ", " + year;
+                        break;
+                    case 2:
+                        selectedDate = "Mar " + dayOfMonth + ", " + year;
+                        break;
+                    case 3:
+                        selectedDate = "Apr " + dayOfMonth + ", " + year;
+                        break;
+                    case 4:
+                        selectedDate = "May " + dayOfMonth + ", " + year;
+                        break;
+                    case 5:
+                        selectedDate = "June " + dayOfMonth + ", " + year;
+                        break;
+                    case 6:
+                        selectedDate = "July " + dayOfMonth + ", " + year;
+                        break;
+                    case 7:
+                        selectedDate = "Aug " + dayOfMonth + ", " + year;
+                        break;
+                    case 8:
+                        selectedDate = "Sept " + dayOfMonth + ", " + year;
+                        break;
+                    case 9:
+                        selectedDate = "Oct " + dayOfMonth + ", " + year;
+                        break;
+                    case 10:
+                        selectedDate = "Nov " + dayOfMonth + ", " + year;
+                        break;
+                    case 11:
+                        selectedDate = "Dec " + dayOfMonth + ", " + year;
+                        break;
+                    default:
+                        selectedDate = "Jan 1, 1970";
+                        break;
+                }
+
+                // Set the text of the TextView to the date
+                date.setText(selectedDate);
+            }
+        });
 
         // Return
         return v;
