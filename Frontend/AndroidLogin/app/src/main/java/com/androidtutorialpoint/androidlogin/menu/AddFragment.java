5aeb3ea364aac68c538acf0cef8bf992a9c6f8db
Jacob Gleason <jggleas@iastate.edu>
2019-03-12 00:50:13 -0500

    created categories dropdown

--- Frontend/AndroidLogin/app/src/main/java/com/androidtutorialpoint/androidlogin/menu/AddFragment.java
+++ Frontend/AndroidLogin/app/src/main/java/com/androidtutorialpoint/androidlogin/menu/AddFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.androidtutorialpoint.androidlogin.R;
 
@@ -34,6 +35,14 @@ public class AddFragment extends Fragment {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
+        // Create an ArrayAdapter using the string array and spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.starting_categories,
+                android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        dropDown.setAdapter(adapter);
 
         // Return
         return v;
