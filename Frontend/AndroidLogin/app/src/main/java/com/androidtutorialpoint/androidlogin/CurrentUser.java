a0f1cad4455246adbe16bde4f3ff1994c906a1b3
Jacob Gleason <jggleas@iastate.edu>
2019-03-12 12:04:23 -0500

    added currentUser class

---
+++ Frontend/AndroidLogin/app/src/main/java/com/androidtutorialpoint/androidlogin/CurrentUser.java
@@ -0,0 +1,22 @@
+package com.androidtutorialpoint.androidlogin;
+
+public class CurrentUser {
+    String id;
+    String token;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
