56aa2a8041534b765d27baf969580970fd460ce1
Jacob Gleason <jggleas@iastate.edu>
2019-03-30 18:02:38 -0500

    updates

--- Frontend/AndroidLogin/app/src/main/java/com/androidtutorialpoint/androidlogin/LoginActivity.java
+++ Frontend/AndroidLogin/app/src/main/java/com/androidtutorialpoint/androidlogin/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.androidtutorialpoint.androidlogin;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.Intent;
-import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,11 +18,8 @@ import com.android.volley.toolbox.StringRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Callable;
 
 public class LoginActivity extends AppCompatActivity {
     // Instance variables
