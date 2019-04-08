5b6f49b301027ce9f721a84e4d215319b30a94d6
Ashley Cockrill <ajc1@iastate.edu>
2019-04-08 00:12:02 -0500

    category changes

--- Backend/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/service/CategoryRepository.java
+++ Backend/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/service/CategoryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.example.ExpensesTracked_Backend.service.imp.Category;
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
-	//public Iterable<Category> findAllBy(String token);
+	public Iterable<Category> findAllByID(int ID);
 }
--- Backend/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/tests/CategoryTest.java
+++ Backend/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/tests/CategoryTest.java
@@ -1,20 +1,62 @@
 package com.example.ExpensesTracked_Backend.tests;
+
 import com.example.ExpensesTracked_Backend.service.imp.Category;
+import com.example.ExpensesTracked_Backend.service.imp.Expenses;
 import com.example.ExpensesTracked_Backend.service.CategoryRepository;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoJUnitRunner;
+import com.example.ExpensesTracked_Backend.service.ExpensesRepository;
+import com.example.ExpensesTracked_Backend.service.ExpensesService;
+import com.example.ExpensesTracked_Backend.service.imp.Category;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Assert;
 import static org.mockito.Mockito.when;
 
-import org.junit.runner.RunWith;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CategoryTest {
-	@Mock
-	CategoryRepository catRepo;
 	
-	@InjectMocks
-	Category cat;
+	@Mock
+	CategoryRepository repo;
+	
+	@Before
+	public void before() 
+	{
+		System.out.println("Before");
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@After
+	public void after()
+	{
+		System.out.println("After");
+	}
+	
+	@Test
+	public void getCategoryByIDTest()
+	{
+		Category cat1 = new Category(1,"category1");
+		Category cat2 = new Category(1,"category2");
+		Category cat3 = new Category(1,"category3");
+
+		List<Category> c = new ArrayList<Category>();
+		c.add(cat1);
+		c.add(cat2);
+		c.add(cat3);
+		
+		when(repo.findAllByID(1)).thenReturn(c);
+		
+		
+		//List<Category> testList =  catserv.getCategoryByID(1);
+		//Assert.assertEquals(c, testList);
+		
+	}
 	
 }
