6ba9b1ca893dc8f19a50898f6af7c0779a076f2d
Jacob Gleason <jggleas@iastate.edu>
2019-04-07 21:33:29 -0500

    updates

--- Frontend/AndroidLogin/app/src/main/java/expensesTracked/RegisterActivity.java
+++ Frontend/AndroidLogin/app/src/main/java/expensesTracked/RegisterActivity.java
@@ -1,11 +1,9 @@
 package expensesTracked;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
