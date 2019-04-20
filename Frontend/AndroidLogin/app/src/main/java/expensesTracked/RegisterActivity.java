c37355a0e859af3775c7db13640125c6931f9a8c
Jacob Gleason <jggleas@iastate.edu>
2019-04-20 00:07:31 -0500

    general updates

--- Frontend/AndroidLogin/app/src/main/java/expensesTracked/RegisterActivity.java
+++ Frontend/AndroidLogin/app/src/main/java/expensesTracked/RegisterActivity.java
@@ -159,6 +159,7 @@ public class RegisterActivity extends AppCompatActivity {
                                 String error_MSG = response.getString("error_msg");
                                 Toast.makeText(getApplicationContext(), error_MSG, Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                hideDialog();
                                 startActivity(intent);
                                 finish();
                             }
