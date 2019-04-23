08baab694482f37f3b0081c458474b5614f21b71
Ty <txtrinh@iastate.edu>
2019-04-23 00:33:51 -0500

    fixed token returning

--- Backend/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/service/imp/User.java
+++ Backend/ExpensesTracked_backend/src/main/java/com/example/ExpensesTracked_Backend/service/imp/User.java
@@ -5,7 +5,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
-public class User {
+public class User implements Cloneable{
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
