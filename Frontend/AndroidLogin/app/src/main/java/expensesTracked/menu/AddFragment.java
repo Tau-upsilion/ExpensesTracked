fb24486936ae70f2b842624e2c5716ed9ed798ca
Jacob Gleason <jggleas@iastate.edu>
2019-05-03 10:05:52 -0500

    updates to code, final commit

--- Frontend/AndroidLogin/app/src/main/java/expensesTracked/menu/AddFragment.java
+++ Frontend/AndroidLogin/app/src/main/java/expensesTracked/menu/AddFragment.java
@@ -1,6 +1,7 @@
 package expensesTracked.menu;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -156,6 +157,10 @@ public class AddFragment extends Fragment {
                             if(!error){
                                 Toast.makeText(getActivity(), "Added income/expense successfully!", Toast.LENGTH_SHORT).show();
                                 hideDialog();
+                                
+//                                Intent goAdd = new Intent(getActivity(), AddFragment.class);
+//                                startActivity(goAdd);
+//                                getActivity().finish();
                             } else {
                                 Toast.makeText(getActivity(), response.getString("error_msg"), Toast.LENGTH_SHORT).show();
                                 hideDialog();
